431c0cfbce30faad9186ed4761ae68e1e4e4ef0e
Gaye Bulut <gayegul@gmail.com>
2015-09-13 21:56:52 -0700

    code code code

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 jQuery(document).ready(function(){
 
+var totalPoints = 0;
+$('#next').hasClass('disabled');
+
+$('#photoOne').click(function(e) {
+  e.preventDefault();
+  totalPoints += 1;
+  console.log(totalPoints);
+  $('next').prop('disabled', false);
+});
+
+$('#photoTwo').click(function(e) {
+  e.preventDefault();
+  totalPoints += 2;
+  console.log(totalPoints);
+  $('next').prop('disabled', false);
+});
+
+$('#photoThree').click(function(e) {
+  e.preventDefault();
+  totalPoints += 3;
+  console.log(totalPoints);
+  $('next').prop('disabled', false);
+});
+
+if(totalPoints < 3) {
+
+}
+
 });
