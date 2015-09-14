f9b0a343417d9e6794d4bd407726b37e7d4c9aa4
Gaye Bulut <gayegul@gmail.com>
2015-09-14 00:18:33 -0700

    complete first draft

--- app.js
+++ app.js
@@ -1,31 +1,53 @@
 jQuery(document).ready(function(){
 
 var totalPoints = 0;
-$('#next').hasClass('disabled');
+var pics = ["url('./pics/IMG_2528.jpg')", "url('./pics/IMG_2572.jpg')", "url('./pics/IMG_3226.jpg')", "url('./pics/IMG_3638.jpg')", "url('./pics/IMG_5771.jpg')", "url('./pics/IMG_3135.jpg')", "url('./pics/IMG_5929.jpg')", "url('./pics/IMG_5963.jpg')", "url('./pics/IMG_5925.jpg')"];
+var picNumber = 2;
 
 $('#photoOne').click(function(e) {
   e.preventDefault();
   totalPoints += 1;
   console.log(totalPoints);
-  $('next').prop('disabled', false);
+  changePic();
 });
 
 $('#photoTwo').click(function(e) {
   e.preventDefault();
   totalPoints += 2;
   console.log(totalPoints);
-  $('next').prop('disabled', false);
+  changePic();
 });
 
 $('#photoThree').click(function(e) {
   e.preventDefault();
   totalPoints += 3;
   console.log(totalPoints);
-  $('next').prop('disabled', false);
+  changePic();
 });
 
-if(totalPoints < 3) {
+function changePic() {
+  $('#photoOne').css('content', pics[++picNumber]);
+  $('#photoTwo').css('content', pics[++picNumber]);
+  $('#photoThree').css('content', pics[++picNumber]);
+  if(picNumber >= pics.length) {
+    showResult();
+    $('#photoOne').off();
+    $('#photoTwo').off();
+    $('#photoThree').off();
+  }
+  //$('#finalResult').empty();
+}
 
+function showResult() {
+  if(totalPoints <= 4) {
+    $('#finalResult').text('You are a true Parisian!');
+  }
+  else if(totalPoints <= 7) {
+    $('#finalResult').text('You are a true American!');
+  }
+  else {
+    $('#finalResult').text('I see some Italian in you!');
+  }
 }
 
 });
